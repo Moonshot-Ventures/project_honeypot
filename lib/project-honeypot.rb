3f93bf1fa260d204225f38adde6f114122ad358b
Guillaume DOTT <guillaume.dott@lafourmi-immo.com>
2012-12-19 09:31:17 +0100

    Add configuration class and modify lookup to use this configuration
    To configure the module, you can use :
    ProjectHoneypot.configure do
      @api_key = 'API_KEY'
    end
    To access, the variable, use :
    ProjectHoneypot.api_key
    
    The lookup method has been modified to be backward compatible
    and use the defined api_key if available.

--- lib/project-honeypot.rb
+++ lib/project-honeypot.rb
@@ -3,8 +3,25 @@ require File.dirname(__FILE__) + "/project_honeypot/url.rb"
 require File.dirname(__FILE__) + "/project_honeypot/base.rb"
 
 module ProjectHoneypot
-  def self.lookup(api_key, url)
-    searcher = Base.new(api_key)
+  class << self
+    attr_accessor :api_key
+
+    def api_key
+      raise "ProjectHoneypot really needs its api_key set to work" unless @api_key
+      @api_key
+    end
+
+    def configure(&block)
+        class_eval(&block)
+    end
+  end
+
+  def self.lookup(api_key_or_url, url=nil)
+    if url.nil?
+      url = api_key_or_url
+      api_key_or_url = ProjectHoneypot.api_key
+    end
+    searcher = Base.new(api_key_or_url)
     searcher.lookup(url)
   end
 end
