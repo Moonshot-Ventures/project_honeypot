a1a7beb464077d8b157c33ef32f997c7e9955d80
Guillaume DOTT <guillaume.dott@lafourmi-immo.com>
2013-01-14 14:30:07 +0100

    Improve tests rspec syntax

--- spec/url_spec.rb
+++ spec/url_spec.rb
@@ -2,53 +2,51 @@ require "spec_helper"
 
 describe ProjectHoneypot::Url do
   describe "with honeypot response" do
-    before(:each) do
-      @url = ProjectHoneypot::Url.new("127.0.0.1", "127.1.63.3")
-    end
+    let(:url) { ProjectHoneypot::Url.new("127.0.0.1", "127.1.63.3") }
 
     it "is safe" do
-      @url.should_not be_safe
+      url.should_not be_safe
 
-      @url.safe?(score: 63).should be_false
-      @url.safe?(score: 64).should be_true
+      url.safe?(score: 63).should be_false
+      url.safe?(score: 64).should be_true
 
-      @url.safe?(last_activity: 1).should be_false
-      @url.safe?(last_activity: 2).should be_true
+      url.safe?(last_activity: 1).should be_false
+      url.safe?(last_activity: 2).should be_true
 
-      @url.safe?(last_activity: 2, score: 64).should be_true
-      @url.safe?(last_activity: 1, score: 64).should be_false
-      @url.safe?(last_activity: 2, score: 63).should be_false
+      url.safe?(last_activity: 2, score: 64).should be_true
+      url.safe?(last_activity: 1, score: 64).should be_false
+      url.safe?(last_activity: 2, score: 63).should be_false
 
-      @url.safe?(offenses: [:comment_spammer]).should be_true
-      @url.safe?(offenses: [:suspicious, :comment_spammer]).should be_false
+      url.safe?(offenses: [:comment_spammer]).should be_true
+      url.safe?(offenses: [:suspicious, :comment_spammer]).should be_false
     end
 
     it "has the correct latest activity" do
-      @url.last_activity.should == 1
+      url.last_activity.should == 1
     end
 
     it "has the correct score" do
-      @url.score.should == 63
+      url.score.should == 63
     end
 
     it "has the correct offenses" do
-      @url.offenses.should include(:suspicious)
-      @url.offenses.should include(:harvester)
-      @url.offenses.should_not include(:comment_spammer)
-      @url.should be_suspicious
-      @url.should be_harvester
-      @url.should_not be_comment_spammer
+      url.offenses.should include(:suspicious)
+      url.offenses.should include(:harvester)
+      url.offenses.should_not include(:comment_spammer)
+      url.should be_suspicious
+      url.should be_harvester
+      url.should_not be_comment_spammer
     end
   end
 
   describe "with search engine honeypot response" do
-    subject { @url = ProjectHoneypot::Url.new("127.0.0.1", "127.0.9.0") }
+    subject { ProjectHoneypot::Url.new("127.0.0.1", "127.0.9.0") }
     it { should be_safe }
     it { should be_search_engine }
   end
 
   describe "with nil honeypot response" do
-    subject { @url = ProjectHoneypot::Url.new("127.0.0.1", nil) }
+    subject { ProjectHoneypot::Url.new("127.0.0.1", nil) }
     it { should be_safe }
   end
 end
